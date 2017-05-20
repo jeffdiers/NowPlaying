253949732731d53b426b8936ceabccc44751a719
jeffdiers <jeff.diers@gmail.com>
2017-05-20 16:01:10 -0600

    Navigation with HomeScreen and ListView components.

--- src/App.js
+++ src/App.js
@@ -3,36 +3,54 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  Button
+} from 'react-native'
+import { StackNavigator } from 'react-navigation'
+import styles from '../styles/GlobalStyle'
 
-export default class NowPlaying extends Component {
+class HomeScreen extends Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  }
+
   render() {
+    const { navigate } = this.props.navigation
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to NowPlaying
         </Text>
+        <Button
+          onPress={() => navigate('List')}
+          title="View movies"
+        />
       </View>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+class ListView extends Component {
+  static navigationOptions = {
+    title: 'Now Playing',
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          List of movies playing now.
+        </Text>
+      </View>
+    )
+  }
+}
+
+const NowPlaying = StackNavigator({
+  Home: { screen: HomeScreen },
+  List: { screen: ListView },
+})
+
+module.exports = NowPlaying
+
+
